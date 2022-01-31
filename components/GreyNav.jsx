e31d2cdc082a43f1111a78f4737a4c1fa501e1e0
Alyssa Cabello <hollenfeuer1@gmail.com>
2022-01-31 16:59:26 -0600

    Blog started

---
+++ components/GreyNav.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GreyNav = () => {
+  return <div></div>;
+};
+
+export default GreyNav;
--- components/index.js
+++ components/index.js
@@ -1,3 +1,6 @@
 export { default as PostCard } from './PostCard';
 export { default as Categories } from './Categories';
 export { default as PostWidget } from './PostWidget';
+export { default as HeaderNav } from './HeaderNav';
+export { default as GreyNav } from './GreyNav';
+export { default as Layout } from './Layout';
